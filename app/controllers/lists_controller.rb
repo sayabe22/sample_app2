23006264094279286f8dad975683d58e1e137065
EC2 Default User <ec2-user@ip-172-31-41-190.ap-northeast-1.compute.internal>
2024-07-13 13:14:00 +0000

    [Add]フラッシュメッセージ

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,8 +6,10 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
+      flash[:notice] = "投稿に成功しました。"
       redirect_to list_path(@list.id)
     else
+      flash.now[:alert] = "投稿に失敗しました。"
       render :new
     end 
   end 
